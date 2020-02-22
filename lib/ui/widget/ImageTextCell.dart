43ca3bcdc286eff503e46964a1e3577af291a06b
Felipe Trova de Araujo <fetrova@gmail.com>
2020-02-22 15:33:29 +0100

    Images until the edge

--- lib/ui/widget/ImageTextCell.dart
+++ lib/ui/widget/ImageTextCell.dart
@@ -11,25 +11,31 @@ class ImageTextCell extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
       child: Column(
         children: <Widget>[
           image,
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              title,
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
             ),
           ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Text(
-              source,
-              style: TextStyle(
-                  fontSize: 14.0,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.normal),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                source,
+                style: TextStyle(
+                    fontSize: 14.0,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.normal),
+              ),
             ),
           )
         ],
