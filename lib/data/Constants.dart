887089031e0034f026f792b52e9fcb0ae9a952ce
Felipe Trova de Araujo <fetrova@gmail.com>
2020-02-23 18:31:58 +0100

    Fetching api key from firebase remote config

--- lib/data/Constants.dart
+++ lib/data/Constants.dart
@@ -2,4 +2,4 @@ final baseUrl = "http://newsapi.org/v2/";
 final topHeadlinesEndpoint = "/top-headlines?language=en";
 
 final authHeader = "Authorization";
-final authKey = "AUTH_KEY";
+final firebaseKey = "api_key";
--- lib/data/NewsRepository.dart
+++ lib/data/NewsRepository.dart
@@ -1,19 +1,45 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_app/data/Constants.dart';
 import 'package:news_app/model/response.dart';
 
 class NewsRepository {
-
-  var _authorizationHeader = {authHeader: authKey};
+  String _authKey;
 
   Future<Response> fetchNews() async {
+    Map<String, String> _authorizationHeader = await _createAuthHeader();
     final fullUrl = baseUrl + topHeadlinesEndpoint;
     var httpResponse = await http.get(fullUrl, headers: _authorizationHeader);
     if (httpResponse.statusCode == 200) {
       return Response.fromJson(httpResponse.body);
     } else {
-      throw Exception("Error fetching data");
+      return Future.error("Error fetching data");
     }
   }
 
+  Future<Map<String, String>> _createAuthHeader() async {
+    if (_authKey == null || _authKey.isEmpty) {
+      _authKey = await getAuthKey();
+      if (_authKey.isEmpty) {
+        Future.error("Error fetching the auth key");
+      }
+    }
+
+    return {authHeader: _authKey};
+  }
+
+  Future<String> getAuthKey() async {
+    RemoteConfig remoteConfig = await _getRemoteConfigInstance();
+    return remoteConfig.getString(firebaseKey);
+  }
+
+  Future<RemoteConfig> _getRemoteConfigInstance() async {
+    final remoteConfig = await RemoteConfig.instance;
+    final defaults = <String, dynamic>{firebaseKey: ""};
+
+    await remoteConfig.setDefaults(defaults);
+    await remoteConfig.fetch(expiration: const Duration(hours: 24));
+    await remoteConfig.activateFetched();
+    return remoteConfig;
+  }
 }
